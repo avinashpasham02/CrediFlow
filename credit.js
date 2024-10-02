a6eb4cdd6669a43c88e25bf52ae1b4e841b6608c
avinash pasham <pashamavinash0@gmail.com>
2024-10-02 07:09:14 +0530

    Create credit.js

---
+++ credit.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const { assessCredit } = require('../controllers/creditController');
+
+// POST request to assess MSME credit
+router.post('/assess', assessCredit);
+
+module.exports = router;
