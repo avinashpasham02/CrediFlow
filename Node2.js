550067dc1a681e4f4f0bbd3142dcd7839207f35d
avinash pasham <pashamavinash0@gmail.com>
2024-10-02 07:07:00 +0530

    Delete Node2.js

--- Node2.js
+++
@@ -1,9 +0,0 @@
-// routes/credit.js
-const express = require('express');
-const router = express.Router();
-const { assessCredit } = require('../controllers/creditController');
-
-// POST request to assess MSME credit
-router.post('/assess', assessCredit);
-
-module.exports = router;
